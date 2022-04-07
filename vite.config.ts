0c68b087f3478766fe698aff3321292340acb05a
link <15896963169@163.com>
2022-04-07 13:43:07 +0800

    fix: 修改 vite 配置

--- vite.config.ts
+++ vite.config.ts
@@ -7,17 +7,8 @@ import path from 'path'
 export default defineConfig({
   base: './',
   resolve: {
-    alias: { // 别名设置
-      '@': path.resolve(__dirname, 'src'),
-      'view': path.resolve(__dirname, 'src/view'),
-      'comp': path.resolve(__dirname, 'src/components'),
-      'api': path.resolve(__dirname, 'src/api'),
-      'router': path.resolve(__dirname, 'src/router'),
-      'store': path.resolve(__dirname, 'src/store'),
-      'plugins': path.resolve(__dirname, 'src/plugins'),
-      'style': path.resolve(__dirname, 'src/style'),
-      'util': path.resolve(__dirname, 'src/util'),
-      'styles': path.resolve(__dirname, 'src/styles')
+    alias: {
+      '@': path.resolve(__dirname, 'src/render')
     },
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.json']
   },
